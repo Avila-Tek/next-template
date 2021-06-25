d1bc62132634c71f62d37b84d3ec669acb7ae062
Jose Roberto Quevedo Gabizon <jose.auevedo2011@gmail.com>
2021-06-25 18:13:33 -0400

    v-1.9.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,43 +3,28 @@ import NProgress from 'nprogress';
 import Router from 'next/router';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
-import { init } from '../lib/sentry';
 import '../styles.css';
 
-if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
-  init();
-}
+function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    Router.events.on('routeChangeStart', () => {
+      NProgress.start();
+    });
 
-Router.events.on('routeChangeStart', () => {
-  NProgress.start();
-});
+    Router.events.on('routeChangeComplete', () => {
+      NProgress.done();
+    });
 
-Router.events.on('routeChangeComplete', () => {
-  NProgress.done();
-});
-
-Router.events.on('routeChangeError', () => {
-  NProgress.done();
-});
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-function MyApp({ Component, pageProps, err }: AppProps) {
+    Router.events.on('routeChangeError', () => {
+      NProgress.done();
+    });
+  }, []);
   return (
     <>
       <Head>
         <title>Plantilla Next.js | Avila Tek</title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,600;1,400&display=swap"
-          rel="preload"
-          as="style"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,600;1,400&display=swap"
-          rel="stylesheet"
-        />
       </Head>
-      <Component {...pageProps} err={err} />
+      <Component {...pageProps} />
     </>
   );
 }
