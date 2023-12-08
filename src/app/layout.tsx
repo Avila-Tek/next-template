0d527f8669f3eb29baeda8f62f9394491c13019c
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2023-12-08 11:58:21 -0400

    feat: :sparkles: sentry added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 import { cn } from '@/lib/utils';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
